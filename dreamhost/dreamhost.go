eaa92136455d38b74f92d350ee837b2b9a92aa6a
Sasha Gerrand <go-dreamhost@sgerrand.com>
2020-01-19 11:07:44 +0000

    Satisfy golint

--- dreamhost/dreamhost.go
+++ dreamhost/dreamhost.go
@@ -6,10 +6,11 @@ import (
 )
 
 const (
-	defaultUrl = "https://api.dreamhost.com/"
+	defaultURL = "https://api.dreamhost.com/"
 	userAgent  = "go-dreamhost"
 )
 
+// A Client manages communication with the Dreamhost API.
 type Client struct {
 	apiKey		string // API key used for communication with the API
 	client		*http.Client // HTTP client used for communication with the API
@@ -17,14 +18,16 @@ type Client struct {
 	UserAgent	string // User agent used for communication with the API
 }
 
-func NewClient(k string, httpClient *http.Client) *Client {
+// NewClient returns a new Dreamhost API client. No checks for a nil apiKey are
+// made. If a nil httpClient is provided, then a new http.Client will be used.
+func NewClient(apiKey string, httpClient *http.Client) *Client {
 	if httpClient == nil {
 		httpClient = &http.Client{}
 	}
 
-	url, _ := url.Parse(defaultUrl)
+	url, _ := url.Parse(defaultURL)
 
-	c := &Client{apiKey: k, client: httpClient, URL: url, UserAgent: userAgent}
+	c := &Client{apiKey: apiKey, client: httpClient, URL: url, UserAgent: userAgent}
 
 	return c
 }
